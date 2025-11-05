f4755fdad03216044adc697abc901fc63e76294a
Walter Pedro <54921496+walter-pedro@users.noreply.github.com>
2025-11-05 14:18:31 -0300

    Changing test class package

--- 0/src/test/java/mockito/CalculadoraTest.java
+++ 0/src/test/java/mockito/CalculadoraTest.java
@@ -1,5 +1,5 @@
-import mockito.Calculadora;
-import mockito.ServicoMatematico;
+package mockito;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
