efa1721a4bbbda899406ccf5fbb52fa5c781fe14
Walter Pedro <54921496+walter-pedro@users.noreply.github.com>
2025-11-05 14:16:20 -0300

    Adding implementation to Exercise 0

--- 0/src/test/java/CalculadoraTest.java
+++ 0/src/test/java/CalculadoraTest.java
@@ -1,11 +1,30 @@
+import mockito.Calculadora;
+import mockito.ServicoMatematico;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 public class CalculadoraTest {
 
     @Mock
     ServicoMatematico servicoMatematico;
 
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     public void deveSomar() {
-        
+        when(servicoMatematico.somar(2, 3)).thenReturn(5);
 
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.somar(2, 3);
+
+        assertEquals(5, resultado);
     }
 }
